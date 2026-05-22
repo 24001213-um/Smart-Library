f4a29625b5688876abaea266aceb374088865ad7
24001213-um <24001213@siswa.um.edu.my>
2026-05-23 07:16:53 +0800

    BorrowStack

---
+++ BorrowStack.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.book;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author sarah
+ */
+ class BorrowStack {
+    private Stack<Book> stack = new Stack<>();
+    
+    public void push (Book b){
+        stack.push(b);
+    }
+    
+    public void show(){
+        if (stack.isEmpty()){
+            System.out.println("History is empty.");
+            return;
+    }
+        System.out.println("\n--- Borrowing History (Most Recent First) ---");
+        //Corrected reverse iteration to show LIFO order properly
+        for (int i = stack.size()-1 ; i >= 0; i-- ){
+            Book b = stack.get(i);
+            System.out.println("[ISBN : " + b.isbn + "] " + b.title + " by " + b.author);
+        }
+    }
+    
+    
+}
